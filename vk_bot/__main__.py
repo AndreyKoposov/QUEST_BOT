82f16752d8d7e8386ec790d5a58f678d6f4e4cd9
AndreyKoposov <140280539+AndreyKoposov@users.noreply.github.com>
2026-04-12 14:43:07 +0300

    structclog bind doesnt works...

--- vk_bot/__main__.py
+++ vk_bot/__main__.py
@@ -1,10 +1,7 @@
-from utils import logger
 from .src.bot import vk
 from .src.routes import labelers
 
 
 if __name__ == "__main__":
-    logger.bind(service='vk_bot')
-
     vk.set_labelers(labelers)
     vk.run()
